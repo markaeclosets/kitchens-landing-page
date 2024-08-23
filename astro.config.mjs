0c3943b7da2840f4b53c6c99b3924edb3e9b555a
= <=>
2024-08-23 11:40:38 -0400

    updates

---
+++ astro.config.mjs
@@ -0,0 +1,17 @@
+import { defineConfig, passthroughImageService } from 'astro/config';
+import tailwind from '@astrojs/tailwind';
+import partytown from '@astrojs/partytown';
+
+import alpinejs from "@astrojs/alpinejs";
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [tailwind(), partytown(), alpinejs()],
+  image: {
+    domains: ['res.cloudinary.com'],
+    service: passthroughImageService()
+  },
+  server: {
+    host: true
+  }
+});
