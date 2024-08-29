5a59b9bc89ab9b83dcd58d8baa61a3cd8c0cc9d1
= <=>
2024-08-29 12:25:56 -0400

    github pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,19 @@
-import { defineConfig, passthroughImageService } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
-import partytown from '@astrojs/partytown';
+import { defineConfig, passthroughImageService } from "astro/config"
+import tailwind from "@astrojs/tailwind"
+import partytown from "@astrojs/partytown"
 
-import alpinejs from "@astrojs/alpinejs";
+import alpinejs from "@astrojs/alpinejs"
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), partytown(), alpinejs()],
   image: {
-    domains: ['res.cloudinary.com'],
+    domains: ["res.cloudinary.com"],
     service: passthroughImageService()
   },
   server: {
     host: true
-  }
-});
+  },
+  site: "https://markaeclosets.github.io",
+  base: "/kitchens-landing-page"
+})
